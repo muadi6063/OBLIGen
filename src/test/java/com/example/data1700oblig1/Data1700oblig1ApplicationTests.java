b39217ceb90c4438324920e4f396b5506b3c7db3
muadi6063 <muadi6063@oslomet.no>
2024-02-18 16:07:27 +0100

    dsds

---
+++ src/test/java/com/example/data1700oblig1/Data1700oblig1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data1700oblig1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data1700oblig1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
